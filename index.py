4ce1023ebc94f61ec15c2b4c94c3eeec975cb700
Icee <102859875+NotIcee12321@users.noreply.github.com>
2023-12-24 03:57:07 +0300

    files
    uploading files

---
+++ index.py
@@ -0,0 +1,31 @@
+def save():
+    name = input("Enter your name: ")
+    file = open("db.txt","w")
+    file.write(name)
+    file.close()
+
+def load():
+    name = input("Enter your name: ")
+    file = open("db.txt","r")
+
+    lines = file.readlines()
+    for i in lines:
+        if i == name:
+            file.close()
+            print("Welcome back " + name)
+        else:
+            print("I dont know " + name)
+
+while True:
+    user_input = input("Are you new(Y/N): ").upper()
+    
+    if user_input == "Y":
+        save()
+    else:
+        load()
+
+    is_continue = input("Do you want to continue?(Y/N): ").upper()
+    if is_continue == "N":
+        break
+    else:
+        pass
